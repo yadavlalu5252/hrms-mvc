b58aa3b073f433078fadba8e55f229937e7736ae
yadavlalu5252 <yadavlalu5252@gmail.com>
2026-09-13 12:19:28 +0530

    auth added

---
+++ hrms-mvc/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hrms_mvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            string? role = HttpContext.Session.GetString("Role");
+
+            if (role != "Admin")
+            {
+                return RedirectToAction(
+                    "Login",
+                    "Auth"
+                );
+            }
+            return View();
+        }
+    }
+}
---
+++ hrms-mvc/Models/EmployeeBankDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hrms_mvc.Models
+{
+    public class EmployeeBankDetails
+    {
+        
+        public int Id { get; set; }
+        public string? BankName { get; set; }
+        public string? AccountNumber { get; set; }
+        public string? IFSCCode { get; set; }
+        public string? BranchName { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
