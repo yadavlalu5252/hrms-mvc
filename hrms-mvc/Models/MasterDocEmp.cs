b88de23b4a67f2f8db13c5d7fd97bbeefd07940d
Shubhams126 <yadavshubhams126@gmail.com>
2026-09-14 12:47:56 +0530

    Added training models

---
+++ hrms-mvc/Models/MasterDocEmp.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hrms_mvc.Models
+{
+    public class MasterDocEmp
+    {
+        public int EmpId { get; set; }
+        [Required(ErrorMessage = "DocumentName is required.")]
+        public string EmpDocName { get; set; }
+
+    }
+}
---
+++ hrms-mvc/Models/Trainers.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hrms_mvc.Models
+{
+    public class Trainers
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "FirsName is compulsory")]
+        public string Fname { get; set; }
+
+        [Required(ErrorMessage = "LastName is compulsory")]
+        public string Lname { get; set; }
+
+        [Required(ErrorMessage = "Role is compulsory")]
+        public string Role { get; set; }
+
+        [Required(ErrorMessage = "Email is compulsory")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Descripion is compulsory")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Status is compulsory")]
+        public string Status { get; set; }
+
+        [Required(ErrorMessage = "ProfilePicture is compulsory")]
+        public string ProfilePicture { get; set; }
+        [Required(ErrorMessage = "Phone is compulsory")]
+        public string? Phone { get; set; }
+    }
+}
